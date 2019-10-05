5d762d8e39c1a52cee485961b7d549fb7004c29b
MATHEUS B GOMES <matheus.bitencourt@yalo.com.br>
2019-10-05 10:47:08 -0300

    Criando a pagina do candidato

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,17 @@ function myFunction() {
         x.classList.add("active");
     }
 }
+
+document.querySelector('.down-arrow').onclick = function () {
+    x = document.querySelector('.vaga-req');
+    if (x.classList.contains('hidden')) {
+        x.classList.remove('hidden');
+    } else x.classList.add('hidden');
+};
+
+document.querySelector('.down-arrow2').onclick = function () {
+    x = document.querySelector('.vaga-req2');
+    if (x.classList.contains('hidden')) {
+        x.classList.remove('hidden');
+    } else x.classList.add('hidden');
+};
